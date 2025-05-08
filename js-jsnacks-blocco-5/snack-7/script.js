00cb094264a8d17348c07e54af016bb5b35043ef
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-05-08 15:40:41 +0200

    feat: snack 7-8 completed

--- js-jsnacks-blocco-5/snack-7/script.js
+++ js-jsnacks-blocco-5/snack-7/script.js
@@ -1,23 +1,32 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
+
+const newArray = [];
+students.forEach((curStudent) => {
+  if (curStudent.id === 2) {
+    newArray.push(curStudent);
+  }
+});
+
+console.log(newArray);
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
